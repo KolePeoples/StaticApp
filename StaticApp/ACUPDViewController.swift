de096dd708cdc65fcfa2dff60b80426d5aecb318
KolePeoples <kolepeoples@gmail.com>
2019-03-03 23:17:21 -0600

    First Commit

---
+++ StaticApp/ACUPDViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ACUPDViewController.swift
+//  StaticApp
+//
+//  Created by Kole Peoples on 3/3/19.
+//  Copyright © 2019 Kole Peoples. All rights reserved.
+//
+
+import UIKit
+
+class ACUPDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- StaticApp/ViewController.swift
+++ StaticApp/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lifesaveImage: UIImageView!
+    
+    @IBAction func yesButton(_ sender: Any) {
+        
+    print("You pressed the yes button")
+    self.performSegue(withIdentifier: "ACUPDSegue", sender: self)
+    }
+    
+    @IBAction func noButton(_ sender: Any) {
+        
+        self.performSegue(withIdentifier: "SOARSegue", sender: self)
+        print("You pressed the no button")
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
